5b08af4894b9de7ad13ccac545692b2f03f8137b
jafar <jafarisaak@gmail.com>
2024-09-06 13:45:51 +0300

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,17 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 //import { fetchRevenue } from '@/app/lib/data';
-import { fetchRevenue,
+import {
          fetchLatestInvoices,
-        fetchCardData,
+         CardsSkeleton,
      } from '@/app/lib/data';
+     import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
@@ -34,9 +35,11 @@ export default async function Page() {
         /> }
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {<RevenueChart revenue={revenue}  /> }
+      <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
         { <LatestInvoices latestInvoices={latestInvoices} /> }
-      </div>
+        </div>
     </main>
   );
 }
