073a624a0f1d623865a7ea77d95a6ab25f830bd3
jafar <tribalchiefjj@gmail.com>
2024-09-06 22:32:26 +0300

    streaming

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,13 +3,10 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 //import { fetchRevenue } from '@/app/lib/data';
-import {
-         fetchLatestInvoices,
-         CardsSkeleton,
-     } from '@/app/lib/data';
+import { fetchLatestInvoices, fetchCardData,} from '@/app/lib/data';
      import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
-
+     import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Card } from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
