f16d30485cb46e0f8a0cf6045889aa40c63eae35
jafar <tribalchiefjj@gmail.com>
2024-09-08 15:59:20 +0300

    improving accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,21 +7,47 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
   });
+
 
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
   const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-  export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
+  export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
+
+  export async function createInvoice(prevState: State, formData: FormData) {  
+    const validatedFields = CreateInvoice.safeParse({  
+            customerId: formData.get('customerId'),
+             amount: formData.get('amount'),
+             status: formData.get('status'),
     });
+
+      // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+          errors: validatedFields.error.flatten().fieldErrors,
+          message: 'Missing Fields. Failed to Create Invoice.',
+        };
+      }
+      // Prepare data for insertion into the database
+      const { customerId, amount, status } = validatedFields.data; 
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -41,13 +67,21 @@ const FormSchema = z.object({
     // Test it out:
    // console.log(rawFormData);
   }
-  export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+  export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({  
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
-  
+
+    if (!validatedFields.success) {
+        return {
+          errors: validatedFields.error.flatten().fieldErrors,
+          message: 'Missing Fields. Failed to Update Invoice.',
+        };
+      }
+    
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
   
     try {
@@ -72,4 +106,7 @@ const FormSchema = z.object({
     } catch (error) {
       return { message: 'Database Error: Failed to Delete Invoice.' };
     }
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
   }
