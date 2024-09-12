de8eb2438d2ec27655d77b219cd16c96d9bf59ce
jafar <tribalchiefjj@gmail.com>
2024-09-12 10:30:35 +0300

    adding authentication

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -9,6 +9,7 @@ export default async function LatestInvoices({
   latestInvoices: LatestInvoice[];
 }) {
   return (
+
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
@@ -20,6 +21,7 @@ export default async function LatestInvoices({
           {latestInvoices.map((invoice, i) => {
             return (
               <div
+              
                 key={invoice.id}
                 className={clsx(
                   'flex flex-row items-center justify-between py-4',
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -6,3 +6,4 @@ export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
   });
+  
